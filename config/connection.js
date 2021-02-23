168405c334f7600e5af1adb28f7b324e283604c6
Luwenxi Song <songl652@newschool.edu>
2021-02-23 03:19:03 -0500

    mysql

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3306,
-  user: 'root',
-  password: 'songluwenxi',
-  database: 'burgers_db',
-});
-
+const connection 
+if (process.env.JAWSDB_URL) {
+  CONNECTION=mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host:'d6rii63wp64rsfb5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user:'buic616fm6vesg5d',
+    password:'mrqkt4tdnt14w9af',
+    database:'l721a8bm0in3qn1l'
+  })
+}
 
 connection.connect((err) => {
   if (err) {
