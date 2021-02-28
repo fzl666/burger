1523d7e67e415f58d2b955e0459323da64202066
Luwenxi Song <songl652@newschool.edu>
2021-02-28 13:34:11 -0500

    fix

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql');
 
-const connection 
+let connection 
 if (process.env.JAWSDB_URL) {
-  CONNECTION=mysql.createConnection(process.env.JAWSDB_URL);
+  connection=mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   connection = mysql.createConnection({
     host:'localhost',
